451c7e39ac8128b6f3a5b8e8bcb981867bc51591
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-08-14 12:42:53 +0530

    eks msg

--- install_eksctl_kubectl.sh
+++ install_eksctl_kubectl.sh
@@ -33,7 +33,7 @@ echo "*************   eksctl installation - start *************"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp &>>$LOGFILE
 VALIDATE $? "Installing eksctl "
 
-sudo mv /tmp/eksctl /usr/local/bin &>>$LOGFILE
+mv /tmp/eksctl /usr/local/bin &>>$LOGFILE
 VALIDATE $? "Moving eksctl from tmp to bin folder"
 
 eksctl version &>>$LOGFILE
